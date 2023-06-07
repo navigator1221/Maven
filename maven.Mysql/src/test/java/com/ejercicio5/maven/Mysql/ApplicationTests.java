be56a8886b643f4aa4b9983ffe30de7545a9712c
navigator1221 <133993107+navigator1221@users.noreply.github.com>
2023-06-07 13:12:43 +0200

    Add files via upload
    Ejercicio 5

---
+++ maven.Mysql/src/test/java/com/ejercicio5/maven/Mysql/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ejercicio5.maven.Mysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
